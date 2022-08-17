e8570b2a45febfdc6f6b477c7014e75edbc2c99c
GutyLabs20 <gutylabs20@gmail.com>
2022-08-17 00:05:24 -0500

    ef-servicio-eureka

---
+++ src/main/java/com/idat/EFJoseLuisGutierrezPizangoEUREKA/EfJoseLuisGutierrezPizangoEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EFJoseLuisGutierrezPizangoEUREKA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfJoseLuisGutierrezPizangoEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfJoseLuisGutierrezPizangoEurekaApplication.class, args);
+	}
+
+}
